d1c7f58909963a3e5512db7e398480cca7d671ed
Louis <jimmy963909827@gmail.com>
2020-12-31 12:09:26 +0800

    Create a new API

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+    }
+    public function login(Request $request)
+    {
+
+    }
+}
